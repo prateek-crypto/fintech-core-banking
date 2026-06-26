3232c7f7b5bad5f006e33c34aa6119162e7bbf20
prateek-crypto <prateekmailg444@gmail.com>
2026-06-27 00:15:30 +0530

    Day 2: Implement Welcome REST API and configure Spring Boot

--- src/main/java/com/prateek/bank/account/AccountServiceApplication.java
+++ src/main/java/com/prateek/bank/account/AccountServiceApplication.java
@@ -2,12 +2,12 @@ package com.prateek.bank.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/prateek/bank/account/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.prateek.bank.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to FinTech Core Banking System";
+    }
+}
